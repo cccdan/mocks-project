9073e9d0ff6940c6e23c8e771373404be28ce724
Dan <cdan700@yahoo.com>
2023-01-14 13:07:49 +0200

    when ... assert fara parametri

--- src/test/java/com/skillsoft/mocks/ArrayListMock.java
+++ src/test/java/com/skillsoft/mocks/ArrayListMock.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ArrayListMock {
 
@@ -15,14 +17,14 @@ public class ArrayListMock {
 
         ArrayList someArrayList = mock(ArrayList.class);
 
-        System.out.println("toString() of mock: " + someArrayList);
-        System.out.println("getClass() of mock: " + someArrayList.getClass());
-        System.out.println("getClass().getSuperClass() of mock: " + someArrayList.getClass().getSuperclass());
+        when(someArrayList.isEmpty()).thenReturn(true);
+        when(someArrayList.size()).thenReturn(23);
+        when(someArrayList.toArray()).thenReturn(new Object[]{1, 3, 6, 9});
 
-        System.out.println("someArrayList instanceof ArrayList: " + (someArrayList instanceof ArrayList));
-        System.out.println("someArrayList instanceof List: " + (someArrayList instanceof List));
-        System.out.println("someArrayList instanceof Collection: " + (someArrayList instanceof Collection));
-        System.out.println("someArrayList instanceof Iterable: " + (someArrayList instanceof Iterable));
-        System.out.println("someArrayList instanceof Object: " + (someArrayList instanceof Object));
+        assertTrue(someArrayList.isEmpty());
+        assertEquals(23, someArrayList.size());
+        assertArrayEquals(new Object[]{1, 3, 6, 9}, someArrayList.toArray());
+
+        System.out.println(someArrayList.toArray());
     }
 }
