06be7aa19c7e219d6560b08486b2fb8ae2a14f69
Dan <cdan700@yahoo.com>
2023-01-14 15:05:11 +0200

    when ... assertLinesmatch

--- src/test/java/com/skillsoft/mocks/ArrayListMock.java
+++ src/test/java/com/skillsoft/mocks/ArrayListMock.java
@@ -3,6 +3,7 @@ package com.skillsoft.mocks;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -17,13 +18,15 @@ public class ArrayListMock {
 
         ArrayList someArrayList = mock(ArrayList.class);
 
-        when(someArrayList.isEmpty()).thenReturn(true);
-        when(someArrayList.size()).thenReturn(23);
-        when(someArrayList.toArray()).thenReturn(new Object[]{1, 3, 6, 9});
+        when(someArrayList.contains("Alice")).thenReturn(true);
+        when(someArrayList.contains("Bob")).thenReturn(false);
 
-        assertTrue(someArrayList.isEmpty());
-        assertEquals(23, someArrayList.size());
-        assertArrayEquals(new Object[]{1, 3, 6, 9}, someArrayList.toArray());
+        when(someArrayList.subList(10, 13)).thenReturn(Arrays.asList("Alice", "Bob", "Charles"));
+
+
+        assertTrue(someArrayList.contains("Alice"));
+        assertFalse(someArrayList.contains("Bob"));
+        assertLinesMatch(Arrays.asList("Alice", "Bob", "Charles"), someArrayList.subList(10, 13));
 
         System.out.println(someArrayList.toArray());
     }
